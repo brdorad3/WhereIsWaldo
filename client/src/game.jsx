61363b00181533dd1cea91a864d87fb82e17e0d8
adis <vunicadis@gmail.com>
2024-04-16 19:15:34 +0200

    Added chars and coordinate

--- client/src/game.jsx
+++ client/src/game.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 function Game() {
-
+    const imageRef = useRef(null);
+    const [coordinates, setCoordinates] = useState({
+        x: '',
+        y: ''
+    });
     const [toggle, setToggle] = useState(false)
     const handleClick = () => {
         setToggle(!toggle)
         console.log(toggle)
     }
+    const handleImageClick = (event) => {
+        
+        if (!imageRef.current) return
+        const rect = imageRef.current.getBoundingClientRect();
+    const x = (event.clientX - rect.left) / imageRef.current.width * imageRef.current.naturalWidth;
+    const y = (event.clientY - rect.top) / imageRef.current.height * imageRef.current.naturalHeight;
+        setCoordinates({
+            x: x.toFixed(0),
+            y: y.toFixed(0)
+        })
+        
+    }
+    useEffect(() => {
+        if(coordinates.x >605 && coordinates.x< 635 && coordinates.y > 275 && coordinates.y < 365){
+            console.log("w")
+        }
+    }, [coordinates]);
+    
 
 return(
 <>
 <p onClick={handleClick} >Zoom in/out</p>
-<img src="wal.webp" className={toggle ? 'img' : 'no-img' } alt="" />
+<img src="walchar2.jpg" alt="" />
+<img onClick={handleImageClick} src="wal2.png" className={toggle ? 'img' : 'no-img' } alt="" onLoad={() => console.log('Image loaded')} ref={imageRef}/>
 
 </>
 )
